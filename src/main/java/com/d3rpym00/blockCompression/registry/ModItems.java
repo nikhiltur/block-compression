7c336027ea16953b919442872949eb320f29925d
d3rpym00 <nikhil.turlapati@gmail.com>
2021-01-24 17:03:36 -0600

    Added custom ItemGroup for the creative menu
    Added a custom creative tab in the creative menu which stores all the compressed blocks.

--- src/main/java/com/d3rpym00/blockCompression/registry/ModItems.java
+++ src/main/java/com/d3rpym00/blockCompression/registry/ModItems.java
@@ -4,15 +4,14 @@ import com.d3rpym00.blockCompression.blockCompression;
 
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     
     // Block Items
-    public static final BlockItem COMPRESSED_DIRT = new BlockItem(ModBlocks.COMPRESSED_DIRT, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
-    public static final BlockItem COMPRESSED_COBBLESTONE = new BlockItem(ModBlocks.COMPRESSED_COBBLESTONE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    public static final BlockItem COMPRESSED_DIRT = new BlockItem(ModBlocks.COMPRESSED_DIRT, new Item.Settings().group(blockCompression.ITEM_GROUP));
+    public static final BlockItem COMPRESSED_COBBLESTONE = new BlockItem(ModBlocks.COMPRESSED_COBBLESTONE, new Item.Settings().group(blockCompression.ITEM_GROUP));
     
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(blockCompression.MOD_ID, "compressed_dirt"), COMPRESSED_DIRT);
